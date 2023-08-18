59ee9af080f23e217563c745ce067fc2858114b4
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-08-18 11:07:45 +0700

    update - login design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,100 +1,90 @@
 import Head from "next/head";
-import {FaLock, FaUserAlt} from "react-icons/fa";
-import {useState} from "react";
+import { FaLock, FaUserAlt } from "react-icons/fa";
 import axios from "axios";
-import {toast} from "react-toastify";
-import {useRouter} from "next/router";
-import {getCookie} from "cookies-next";
+import { useRouter } from "next/router";
+import { getCookie } from "cookies-next";
+import { useForm } from "react-hook-form";
+import { showErrorToast, showSuccessToast } from "@/utils/toast";
+import Image from "next/image";
 
 export default function Index() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const {
+        register,
+        handleSubmit,
+    } = useForm(); // Inisialisasi react-hook-form
+    const router = useRouter(); // Menggunakan router Next.js
 
-    const router = useRouter()
-
-    const handleLogin = async (e) =>{
-        e.preventDefault();
+    // Fungsi yang dipanggil saat formulir login di-submit
+    const handleLogin = async (data) => {
         try {
-            await axios.post('api/auth/login',{
-                username : username,
-                password : password,
-            }).then(() =>{
-                toast.success("Login Sukses", {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                });
-                router.push('/home')
-            })
+            // Melakukan permintaan ke endpoint login
+            await axios.post('api/auth/login', data);
+            showSuccessToast('Login Berhasil'); // Menampilkan pesan sukses
+            await router.replace('/home'); // Mengarahkan pengguna ke halaman home setelah login
         } catch (e) {
-            toast.error(e.response.data['data'], {
-                position: "top-right",
-                autoClose: 2000,
-                hideProgressBar: true,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "colored",
-            });
+            showErrorToast(e.response.data['data']); // Menampilkan pesan error jika login gagal
         }
     }
+
     return (
         <>
             <Head>
                 <title>Login</title>
             </Head>
-            <div className={`min-h-screen w-full bg-[#051467de] flex flex-col items-center`}>
-                <div className={`w-[35%] h-full bg-white rounded-xl mt-[5cm] py-4`}>
-                    <div className={`bg-[#09209f] w-[260px] `}>
-                        <h2 className={`text-white text-[1.2em] font-semibold rounded-br rounded-tr py-[10px] pr-[5px] pl-[27px]`}>
-                            PT VUTEQ INDONESIA
+            <div className="min-h-screen w-full bg-[#051467de] flex justify-center flex-col items-center">
+                <div className="md:w-[30%] h-full bg-white rounded-l p-5">
+                    <div className="flex flex-row">
+                        <Image src="/logo.png" alt="Logo" width={120} height={80} />
+                        <h2 className="text-[1.2em] font-bold rounded-br rounded-tr py-[10px] pr-[5px] pl-[27px]">
+                            HPM Outgoing System
                         </h2>
                     </div>
-                    <div className={`p-5 mt-8`}>
+                    <form onSubmit={handleSubmit(handleLogin)} className="mt-8">
+                        {/* Input untuk username */}
                         <div className="flex items-center border border-gray-300 rounded-lg py-3 px-4">
                             <div className="mr-2">
                                 <FaUserAlt />
                             </div>
                             <input
-                                onChange={(e)=> setUsername(e.currentTarget.value)}
+                                {...register('username')} // Menghubungkan input dengan react-hook-form
                                 className="focus:outline-none w-full border-none outline-none focus:border-none" type="text" placeholder="Username" />
                         </div>
-                        <br/>
+                        <br />
+                        {/* Input untuk password */}
                         <div className="flex items-center border border-gray-300 rounded-lg py-3 px-4">
                             <div className="mr-2">
-                               <FaLock />
+                                <FaLock />
                             </div>
                             <input
-                                onChange={(e)=> setPassword(e.currentTarget.value)}
+                                required
+                                {...register('password')} // Menghubungkan input dengan react-hook-form
                                 className="focus:outline-none w-full border-none outline-none focus:border-none" type="password" placeholder="Password" />
                         </div>
+                        {/* Tombol untuk submit form login */}
                         <button
-                            onClick={handleLogin}
-                            className={`w-full bg-[#09209f] py-4 text-white text-center text-xl mt-7 rounded-b-xl`}>
+                            type="submit"
+                            className="w-full bg-blue-600 hover:bg-blue-900 py-4 text-white text-center text-xl mt-7 rounded-b-xl">
                             Login
                         </button>
-                    </div>
+                    </form>
                 </div>
-                <p className={`text-white mt-8`}>
-                    Copyright © 2023
+                {/* Informasi hak cipta dan versi aplikasi */}
+                <p className="text-white mt-8">
+                    PT VUTEQ INDONESIA © 2023
                 </p>
-                <p className={`text-white mt-2`}>
-                    Version 1.1.0
+                <p className="text-white mt-2">
+                    Versi 1.0.1
                 </p>
             </div>
         </>
     )
 }
 
+// Fungsi untuk mendapatkan props server-side
 export const getServerSideProps = ({ req, res }) => {
-    const cookie = getCookie('@vuteq-1-token', { req, res });
+    const cookie = getCookie('@vuteq-token', { req, res });
 
+    // Jika cookie sudah ada (pengguna sudah login), maka arahkan ke halaman home
     if (cookie) {
         res.writeHead(302, { Location: '/home' });
         res.end();
